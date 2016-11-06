ae06114e39db62057578c1b34e5def7f57b7de1b
U-ACCOUNTS\rschuckit <ACCOUNTS+rschuckit@URB222-26.seasad.wustl.edu>
2016-11-05 21:14:25 -0500

    Reading in from terms list correctly

--- translate_script.js
+++ translate_script.js
@@ -1,5 +1,26 @@
 //document.body.innerHTML = document.body.innerHTML.replace(new RegExp("the", "g"), "Ruben");
 
+var req = new XMLHttpRequest();  
+req.open('GET', 'http://www.aboutruben.com/termlist.txt', false);   
+req.send(null);  
+
+let terms = "";
+
+if(req.status == 200)  
+  terms = req.responseText;
+
+const termLines = terms.split('\n');
+
+let dictionary = {};
+
+for(let i = 0; i < termLines.length; ++i) {
+	const line = termLines[i];
+	const termParts = line.split('–');
+	dictionary[termParts[0]] = termParts[1];
+}
+
+alert(dictionary);
+
 var replaceTextInNode = function(parentNode){
     for(var i = parentNode.childNodes.length-1; i >= 0; i--){
         var node = parentNode.childNodes[i];
@@ -7,7 +28,8 @@ var replaceTextInNode = function(parentNode){
         //  Make sure this is a text node
 
         if(node.nodeType == Element.TEXT_NODE){
-            node.textContent = node.textContent.replace("the", "Ruben!");
+			const regex = /(the)\b/gi;
+            node.textContent = node.textContent.replace(regex, "Ruben!");
         } else if(node.nodeType == Element.ELEMENT_NODE){
             //  Check this node's child nodes for text nodes to act on
             replaceTextInNode(node);
