af552dfad9a6804c8d749b871340605aac16c00c
U-ACCOUNTS\rschuckit <ACCOUNTS+rschuckit@URB222-26.seasad.wustl.edu>
2016-11-05 19:02:58 -0500

    Added icon to dir and termlist

--- translate_script.js
+++ translate_script.js
@@ -1,2 +1,18 @@
-alert("Running the thing");
-document.body.innerHTML = document.body.innerHTML.replace(new RegExp("the", "g"), "Ruben");
+//document.body.innerHTML = document.body.innerHTML.replace(new RegExp("the", "g"), "Ruben");
+
+var replaceTextInNode = function(parentNode){
+    for(var i = parentNode.childNodes.length-1; i >= 0; i--){
+        var node = parentNode.childNodes[i];
+
+        //  Make sure this is a text node
+
+        if(node.nodeType == Element.TEXT_NODE){
+            node.textContent = node.textContent.replace("the", "Ruben!");
+        } else if(node.nodeType == Element.ELEMENT_NODE){
+            //  Check this node's child nodes for text nodes to act on
+            replaceTextInNode(node);
+        }
+    }
+};
+
+replaceTextInNode(document.body);
